fecfc6d65eaa48f5b124618248d975bd9130b76b
977011524 <977011524@qq.com>
2021-01-20 13:55:21 +0800

    授权

---
+++ auth.php
@@ -0,0 +1,21 @@
+<?php
+
+//生成授权
+$uid = 123;
+$user_name = 'lisi';
+
+$token1=sha1($uid.$user_name.mt_rand(1,999999),time());
+$token2 = strrev($token1);
+
+$respomse = [
+    'error'=>0,
+    'msg'=>"ok",
+    'data' => [
+        'token'=>$token2
+        ]
+    ];
+
+    echo json_encode($respomse);
+
+
+?>
