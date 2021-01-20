60154e6c8c88d5578b253386ca7193dfbcc27393
977011524 <977011524@qq.com>
2021-01-20 14:02:34 +0800

    授权

--- auth.php
+++ auth.php
@@ -1,21 +1,18 @@
 <?php
 
 //生成授权
-$uid = 123;
+$uid =123;
 $user_name = 'lisi';
 
-$token1=sha1($uid.$user_name.mt_rand(1,999999),time());
+$token1 =sha1($uid.$user_name.mt_rand(1,999999).time());
 $token2 = strrev($token1);
 
-$respomse = [
-    'error'=>0,
-    'msg'=>"ok",
-    'data' => [
-        'token'=>$token2
-        ]
-    ];
+$response =[
+    'errno'=>0,
+    'msg'=> 'ok',
+    'data' =>[
+        'token' =>$token2
+    ]
+];
 
-    echo json_encode($respomse);
-
-
-?>
+echo json_encode($response);
